59a145dffa8b932ac5b7a4d59aee5e67112b1230
smr24425 <smr24425@gmail.com>
2025-07-06 15:50:44 +0800

    fixed

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,12 +2,12 @@
 <template>
   <nav class="flex items-center justify-between bg-black text-white p-4">
     <div class="text-xl md:text-3xl font-bold">Brown Dust 2 L2D Viewer</div>
-    <a href="https://github.com/Jelosus2/BD2-L2D-Viewer" target="_blank">
+    <a href="https://github.com/smr24425/BD2-Viewer" target="_blank">
       <GithubIcon class="w-5 h-5 md:w-7 md:h-7" />
     </a>
   </nav>
 </template>
 
 <script setup lang="ts">
-import GithubIcon from '@/components/icons/GithubIcon.vue';
+import GithubIcon from "@/components/icons/GithubIcon.vue";
 </script>
